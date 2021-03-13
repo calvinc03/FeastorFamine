47582007b3a0e81ffc1027f3a592a35d31342a45
Seiji <seijiwjsmith@gmail.com>
2021-03-12 22:19:53 -0800

    refactored spider rig
    - parent method

--- src/spider.cpp
+++ src/spider.cpp
@@ -3,48 +3,55 @@
 #include "spider.hpp"
 #include "mob.hpp"
 
+
+//TODO
+//void animate(time_elapsed) -- interpolate keyframes.
+//generalize rig. 
+
+
 // this function creates a hierarchy through accumulating transforms.
 void Spider::update_rigs() {
     auto view_parts = registry.view<Spider, Motion>();
-    for (auto [entity,spider, motion] : view_parts.each()) { // motion == root
+    for (auto [entity,spider, root_motion] : view_parts.each()) { // motion == root
 
         auto& body_motion = registry.get<Motion>(spider.body);
         auto& L_upper_leg_motion = registry.get<Motion>(spider.L_upper_leg);
         auto& L_lower_leg_motion = registry.get<Motion>(spider.L_lower_leg);
-        
-
-        //test code
-        L_upper_leg_motion.position = vec2(50, 0); //offset should be an argument for part.
-        L_lower_leg_motion.angle += 0.1f; // keyframes are an array of angles. 
-        L_upper_leg_motion.angle += 0.2f;
+        auto& R_upper_leg_motion = registry.get<Motion>(spider.R_upper_leg);
+        auto& R_lower_leg_motion = registry.get<Motion>(spider.R_lower_leg);
 
         // create kinematic chain via transforms
         auto& body_transform = registry.get<Transform>(spider.body);
         auto& L_upper_leg_transform = registry.get<Transform>(spider.L_upper_leg);
         auto& L_lower_leg_transform = registry.get<Transform>(spider.L_lower_leg);
+        auto& R_upper_leg_transform = registry.get<Transform>(spider.R_upper_leg);
+        auto& R_lower_leg_transform = registry.get<Transform>(spider.R_lower_leg);
 
+        //test code
+        L_lower_leg_motion.angle = 3.14 / 2.0f; // keyframes are an array of angles. 
+        L_upper_leg_motion.angle += 0.2f;
+        R_lower_leg_motion.angle = 3.14 / 2.0f; 
+        R_upper_leg_motion.angle -= 0.2f;
+
+        //body becomes the root.
         body_transform.mat = glm::mat3(1.0);
-        body_transform.translate(motion.position); //Note: can't have a body offset doing this.
-        body_transform.rotate(motion.angle);
+        body_transform.translate(root_motion.position);
+        body_transform.rotate(root_motion.angle);
+
+        //create parent constraints
+        L_upper_leg_transform = parent(body_transform, L_upper_leg_motion, vec2(0, 70));
+        L_lower_leg_transform = parent(L_upper_leg_transform, L_lower_leg_motion, vec2(0, -70));
+
+        R_upper_leg_transform = parent(body_transform, R_upper_leg_motion, vec2(0, 70));
+        R_lower_leg_transform = parent(R_upper_leg_transform, R_lower_leg_motion, vec2(0, -70));
+
         
-        L_upper_leg_transform.mat = glm::mat3(1.0);
-        L_upper_leg_transform.translate(L_upper_leg_motion.position);
-        L_upper_leg_transform.translate(vec2(0, 70)); //TODO: these magic numbers are the pivot of rotation. should scale with motion.scale.
-        L_upper_leg_transform.rotate(L_upper_leg_motion.angle);
-        L_upper_leg_transform.translate(vec2(0, -70));
-        L_upper_leg_transform.mat = body_transform.mat * L_upper_leg_transform.mat; //this applies parent's transforms to child
-
-        L_lower_leg_transform.mat = glm::mat3(1.0);
-        L_lower_leg_transform.translate(L_lower_leg_motion.position);
-        L_lower_leg_transform.translate(vec2(0, -70));
-        L_lower_leg_transform.rotate(L_lower_leg_motion.angle);
-        L_lower_leg_transform.translate(vec2(0, 70));
-        L_lower_leg_transform.mat =  L_upper_leg_transform.mat * L_lower_leg_transform.mat;
-
         //must adjust scale after!!
-        body_transform.scale(motion.scale*body_motion.scale); //TODO, make sure this makes sense
-        L_upper_leg_transform.scale(motion.scale * L_upper_leg_motion.scale);
-        L_lower_leg_transform.scale(motion.scale * L_lower_leg_motion.scale);
+        body_transform.scale(root_motion.scale*body_motion.scale);
+        L_upper_leg_transform.scale(root_motion.scale * L_upper_leg_motion.scale);
+        L_lower_leg_transform.scale(root_motion.scale * L_lower_leg_motion.scale);
+        R_upper_leg_transform.scale(root_motion.scale * R_upper_leg_motion.scale);
+        R_lower_leg_transform.scale(root_motion.scale * R_lower_leg_motion.scale);
 
     }
 }
@@ -56,11 +63,15 @@ entt::entity  Spider::createSpider() {
     auto body = createSpiderPart("body");
     auto L_upper_leg = createSpiderPart("rect7", { 50,0 });
     auto L_lower_leg = createSpiderPart("rect7");
+    auto R_upper_leg = createSpiderPart("rect7", { -50,0 });
+    auto R_lower_leg = createSpiderPart("rect7");
 
     auto& spider = registry.emplace<Spider>(entity);
     spider.body = body;
     spider.L_lower_leg = L_lower_leg;
     spider.L_upper_leg = L_upper_leg;
+    spider.R_lower_leg = R_lower_leg;
+    spider.R_upper_leg = R_upper_leg;
 
     // acts like any other entity.
     auto& motion = registry.emplace<Motion>(entity);
@@ -97,4 +108,15 @@ entt::entity Spider::createSpiderPart(std::string name, vec2 offset)
     registry.emplace<Transform>(entity);
 
     return entity;
+}
+
+Transform parent(Transform parent, Motion child_motion, vec2 origin_offset) {
+    Transform child;
+    child.mat = glm::mat3(1.0);
+    child.translate(child_motion.position);
+    child.translate(origin_offset); //TODO: these magic numbers are the pivot of rotation. should scale with motion.scale.
+    child.rotate(child_motion.angle);
+    child.translate(-origin_offset);
+    child.mat = parent.mat * child.mat; //this applies parent's transforms to child
+    return child;
 }
--- src/spider.hpp
+++ src/spider.hpp
@@ -4,13 +4,13 @@
 #include "entt.hpp"
 #include "grid_map.hpp"
 #include <vector>
-// Creates all the associated render resources and default transform
 
 struct Spider
 {
 	static entt::entity createSpider();
 	static entt::entity createSpiderPart(std::string name, vec2 offset = {0,0}); //, vec2 offset, vec2 scale
 
+	//could make jagged 2D vector to store a series of kinematic chains, generalize to rig class. 
 	entt::entity body;
 	entt::entity L_upper_leg;
 	entt::entity L_lower_leg;
@@ -20,3 +20,6 @@ struct Spider
 	//apply transforms hierarchially. should be called every world step. could be in another file like animation.cpp
 	static void update_rigs();
 };
+
+Transform parent(Transform parent, Motion child_motion, vec2 origin_offset);
+
