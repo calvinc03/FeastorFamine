5e3aaf50906f7d9aaf0ec93d304114f5287de854
calvinc03 <43226170+calvinc03@users.noreply.github.com>
2021-04-07 13:06:47 -0700

    damage change

--- src/config/unit_config.hpp
+++ src/config/unit_config.hpp
@@ -8,7 +8,7 @@ const std::map<unit_type, int> unit_damage = {
 	//{WATCHTOWER, 10},
 	{GREENHOUSE, 60},
 	{EXTERMINATOR, 10},
-	{ROBOT, 1},
+	{ROBOT, 10},
 	{PRIESTESS, 5},
 	{SNOWMACHINE, 50},
 	{WALL, 0}
