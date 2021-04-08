6617d2e905a2281bba6f4c6eff9cf5f4eb8968e9
Darren <lvaldi@students.cs.ubc.ca>
2021-04-07 17:48:38 -0700

    heat shader code

--- data/shaders/water.fs.glsl
+++ data/shaders/water.fs.glsl
@@ -8,6 +8,15 @@ in vec2 texcoord;
 
 layout(location = 0) out vec4 color;
 
+float min_dist_to_edge(vec2 uv) {
+	float dist_to_top = uv.y;
+	float dist_to_bottom = 1.0 - uv.y;
+	float dist_to_left = uv.x;
+	float dist_to_right = 1.0 - uv.x;
+
+	return min(dist_to_top, min(dist_to_bottom, min(dist_to_left, dist_to_right)));
+}
+
 vec2 distort(vec2 uv)
 {
 	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -15,7 +24,16 @@ vec2 distort(vec2 uv)
 	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 	vec2 coord = uv.xy;
-	return coord;
+	coord += 0.02*vec2(sin(0.05*time + 1.*uv.y), cos(1234. + 0.025*time + 2.*uv.x));
+	coord += 0.01*vec2(sin(10.*(0.05*time + 1.*uv.y)), cos(10.*(1234. + 0.025*time + 2.*uv.x)));
+	
+	vec2 from = uv;
+	vec2 to = coord;
+	float fraction = clamp(10.0 * min_dist_to_edge(texcoord), 0.0, 1.0);
+
+	return mix(from, to, fraction);
+
+	// return coord;
 }
 
 vec4 color_shift(vec4 in_color)
@@ -24,8 +42,16 @@ vec4 color_shift(vec4 in_color)
 	// HANDLE THE COLOR SHIFTING HERE (you may want to make it blue-ish)
 	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-	vec4 color = in_color;
+	 vec4 color = mix(in_color, vec4(0.1, 0.1, 0.95, 1.0), 0.2);
+	
+//	vec4 from = vec4(1.0, 0.0, 0.0, 1.0);
+//  vec4 to = color;
+//	float fraction = clamp(10.0 * min_dist_to_edge(texcoord), 0.0, 1.0);
+
+//	return mix(from, to, fraction);
+
 	return color;
+
 }
 
 vec4 fade_color(vec4 in_color)
