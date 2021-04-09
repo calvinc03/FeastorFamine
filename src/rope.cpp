573bff7a523b250a808a25fc20c62a176fc8b74e
Seiji <seijiwjsmith@gmail.com>
2021-04-09 00:42:57 -0700

    rope update
    - will add to dragon

--- src/rope.cpp
+++ src/rope.cpp
@@ -1,38 +1,54 @@
 #include "rope.hpp"
 #include <iostream>
-const std::string link= "monsters/dragon_rig/chain_link.png";
 
-entt::entity RopeRig::createRope(vec2 start, int length) {
+
+//const std::string link= "monsters/dragon_rig/chain_link.png";
+const std::string link_top = "monsters/dragon_rig/chain_top.png";
+const std::string link_side = "monsters/dragon_rig/chain_side.png";
+
+entt::entity RopeRig::createRope(vec2 start, int length, vec2 offset) {
     auto entity = registry.create();
     auto& rope = registry.emplace<RopeRig>(entity);
 
     for (int i = 0; i < length; i++) {
-        rope.chain.push_back(createRopePart(start + vec2(45 * i, 40*i))); // add links to a vector
+        if (i % 2 == 0) {
+            rope.chain.push_back(createRopePart(start + vec2(45 * i, 40 * i), link_top)); // add links to a vector
+        }
+        else {
+            rope.chain.push_back(createRopePart(start + vec2(45 * i, 40 * i), link_side)); // add links to a vector
+        }
+            
     }
+    rope.anchor = rope.chain[0];
+    rope.offset = offset;
 
     update_rig(entity); //initialize rig
     return entity;
 }
 
-entt::entity RopeRig::createRopePart(vec2 pos) {
+entt::entity RopeRig::createRopePart(vec2 pos, std::string name) {
     
     auto entity = registry.create();
 
-    std::string key = link;
+    std::string key = name;
     ShadedMesh& resource = cache_resource(key);
     if (resource.effect.program.resource == 0)
     {
         resource = ShadedMesh();
-        RenderSystem::createSprite(resource, textures_path(link), "monster");
+        RenderSystem::createSprite(resource, textures_path(name), "monster");
     }
 
 
     ShadedMeshRef& mesh_ref = registry.emplace<ShadedMeshRef>(entity, resource);
-    mesh_ref.layer = 24;
 
+    if(name == link_side)
+        mesh_ref.layer = 24;
+    else 
+        mesh_ref.layer = 23;
+ 
     auto& motion = registry.emplace<Motion>(entity);
     motion.angle = 0.0f;
-    motion.velocity = { 0, 0 };
+    motion.velocity = { 0, 500 };
     motion.scale = vec2(50,50); 
     motion.position = pos;
     //motion.scale.y *= -1;
@@ -57,7 +73,7 @@ void update_helper(RopeRig ropeRig) {
         float dist = length(p1p0);
 
         //update dist
-        float dif = abs(dist - 40.0f);
+        float dif = abs(dist - 20.0f);
         link0_motion.position = link0_motion.position + dir * dif / 2.0f;
         link1_motion.position = link1_motion.position - dir * dif / 2.0f;
         
