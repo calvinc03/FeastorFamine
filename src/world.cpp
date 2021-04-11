15f57fb9292f1d1fc06de97c8a73981928b0f1de
June <junewu.cs@gmail.com>
2021-04-11 01:32:49 -0700

    bug fix

--- src/world.cpp
+++ src/world.cpp
@@ -2528,9 +2528,8 @@ void WorldSystem::start_menu_click_handle(double mouse_pos_x, double mouse_pos_y
 		case (MenuButtonType::new_game_button):
 			remove_menu_buttons();
 //			game_state = help_menu;
+//          RenderSystem::show_entity(help_menu_entity);
 			restart_with_save();
-			// show controls overlay
-			RenderSystem::show_entity(help_menu_entity);
 			break;
 		case (MenuButtonType::load_game_button):
 			remove_menu_buttons();
