47f9c7f192d0003e5478ada64b57074d3a717c15
June-Wu <june.wu.cs@gmail.com>
2021-04-12 21:28:14 -0700

    2x speed in survival fix

--- src/world.cpp
+++ src/world.cpp
@@ -3256,7 +3256,7 @@ void WorldSystem::in_game_click_handle(double xpos, double ypos, int button, int
 
 		Priestess::updateBuffs();
 	}
-	else if (player_state == battle_stage)
+	if (player_state == battle_stage)
 	{
 		if (ui_button == Button::fastforward_button)
 		{
